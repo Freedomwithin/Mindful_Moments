14c7163058497048d75bf9547495f921f367c98a
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-24 15:24:10 -0500

    recent migrations

--- models.py
+++ models.py
@@ -1,12 +1,25 @@
 from extensions import db
 from flask_login import UserMixin
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash  # Import security functions
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)  # Add password_hash column
     journal_entries = db.relationship('JournalEntry', backref='author', lazy=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow) # Add created_at column
+
+    @property
+    def password(self):
+        raise AttributeError('password is a write-only attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class JournalEntry(db.Model):
     id = db.Column(db.Integer, primary_key=True)

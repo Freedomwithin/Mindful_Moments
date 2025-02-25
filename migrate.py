7f231ef9503a1fedb9d1a15b815d49cb34738de7
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-24 21:41:16 -0500

    - python migrate.py  added tp render yaml

--- migrate.py
+++ migrate.py
@@ -1,12 +1,14 @@
 from app import create_app
 from app.extensions import db  # Import db from extensions
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     with app.app_context():
-        # Add any migration commands you need here, e.g.:
-        from flask_migrate import upgrade
+        # Create tables (if they don't exist)
+        db.create_all()
+
+        # Apply any pending migrations
         upgrade()

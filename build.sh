ad6c1b341afbb1c91ad210af8eb366dadf6ce766
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-25 04:02:35 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status.
 
-# Activate the virtual environment (if you're using one)
-source.venv/bin/activate
-
 # Install dependencies
 pip install -r requirements.txt
 

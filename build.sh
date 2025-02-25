95d72ace97e1ad99a453912aedb51cbc9e06c0f9
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-25 16:28:53 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,6 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 # Install dependencies
 pip install -r requirements.txt
 
-#Attemp to rm migatations on render
-rm -rf migrations 
-rm instance/your_database.db
-
 # Database migrations
 flask db init 
 flask db migrate

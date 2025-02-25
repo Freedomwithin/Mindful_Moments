3a25d312c7d4646fad882b49d0b3640c8a19646b
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-25 18:59:40 -0500

    build.sh update

---
+++ app/static/update_alembic_version.sql
@@ -0,0 +1,1 @@
+INSERT INTO alembic_version (version_num) VALUES ('d1a502d2470b');
--- build.sh
+++ build.sh
@@ -4,7 +4,10 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 # Install dependencies
 pip install -r requirements.txt
 
+# Update Alembic version
+psql $DATABASE_URL -f update_alembic_version.sql
+
 # Database migrations
 flask db init
 flask db migrate
-flask db upgrade 
+flask db upgrade

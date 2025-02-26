d3bddb702b1eb54c6fd60662ffef737cf8c0b2c6
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-25 19:55:58 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,8 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 # Install dependencies
 pip install -r requirements.txt
+
+# Database migrations
+flask db init
+flask db migrate
+flask db upgrade

9c8150e8652c0f89d28684a8c95123f73d3964fe
VirtualMachine <166790647+Freedomwithin@users.noreply.github.com>
2025-02-25 19:08:26 -0500

    build.sh change

--- build.sh
+++ build.sh
@@ -4,9 +4,6 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 # Install dependencies
 pip install -r requirements.txt
 
-# Update Alembic version
-psql $DATABASE_URL -f update_alembic_version.sql
-
 # Database migrations
 flask db init
 flask db migrate
